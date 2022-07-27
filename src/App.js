9b086ea745f1228a66b10f2da4ac1e101038a073
gsPurple <goncalosroxo@gmail.com>
2022-07-27 23:58:48 +0100

    removed stop anim on resize to fix current page anim

--- src/App.js
+++ src/App.js
@@ -5,14 +5,7 @@ import Projects from "./pages/Projects";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
-  let resizeTimer;
-  window.addEventListener("resize", () => {
-    document.body.classList.add("resize-animation-stopper");
-    clearTimeout(resizeTimer);
-    resizeTimer = setTimeout(() => {
-      document.body.classList.remove("resize-animation-stopper");
-    }, 400);
-  });
+  
 
   return (
     <div className="container">
