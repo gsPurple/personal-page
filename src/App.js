affd7397a1c57d448ec3d6315f28b52c8cef82c9
gsPurple <goncalosroxo@gmail.com>
2022-07-28 23:26:38 +0100

    contact link placeholder page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import CV from "./pages/CV";
 import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/cv' element={<CV />} />
         <Route path='/projects' element={<Projects />} />
+        <Route path='/contact' element={<Contact />} />
       </Routes>
     </div>
   );
---
+++ src/pages/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Contact = () => {
+    return(
+        <div>
+            <section className='section-container'>
+                <div id='welcome-img'>
+                    <h1>MY Contact</h1>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Contact
