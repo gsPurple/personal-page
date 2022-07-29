81668c7df2a730cf1da8e2b50a169df53c1b1b40
gsPurple <goncalosroxo@gmail.com>
2022-07-30 00:28:16 +0100

    added custom 404 place holder with routes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import Home from "./pages/Home";
 import CV from "./pages/CV";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
+import PageNotFound from "./pages/PageNotFound";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   
-
   return (
     <div className="container">
       <Header/>
@@ -16,6 +16,7 @@ function App() {
         <Route path='/cv' element={<CV />} />
         <Route path='/projects' element={<Projects />} />
         <Route path='/contact' element={<Contact />} />
+        <Route path='*' element={<PageNotFound/>} />
       </Routes>
     </div>
   );
---
+++ src/pages/PageNotFound.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const PageNotFound = () => {
+    return(
+        <div>
+            <section className='section-container'>
+                <div id='welcome-img'>
+                    <h1>404</h1>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default PageNotFound
