aa028c0fd51a8ae36225c47325773214e7e77ba1
gsPurple <goncalosroxo@gmail.com>
2022-09-18 22:42:02 +0100

    test token

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -5,7 +5,7 @@ const PageNotFound = () => {
         <div>
             <section className='section-container'>
                 <div id='welcome-img'>
-                    <h1>404</h1>
+                    <h1>404 - Ooops! This is not the page you were looking for...</h1>
                 </div>
             </section>
         </div>
