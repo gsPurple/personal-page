679c1cdce0b3feab02fb6173d5086b0595965cbd
gsPurple <goncalosroxo@gmail.com>
2023-07-18 19:49:06 +0100

    pls

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
                 <div id='intro-text'>
                     <h1>Hello, world!</h1>
                     I'm Gonçalo <span className='light-purp'>Roxo</span>, welcome to my corner of the internet.
-                    I'm currently looking for new opportunities to develop my skills! sjfhsdkjfhdjk
+                    I'm currently looking for new opportunities to develop my skills!
                     <p>Stay awhile and get to know me!</p>
                 </div>
                 {/*<a href="https://www.freepik.com/vectors/geography">Geography vector created by brgfx - www.freepik.com</a>*/}
